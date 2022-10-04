c7b96c8a43d8792b65508d67adff636f6ca0c5c5
moghatt <mohammad_ghatali_kh@yahoo.com>
2022-10-05 09:52:50 +1030

    aa

--- client/js/components/sign_up.js
+++ client/js/components/sign_up.js
@@ -1,45 +1,92 @@
 function renderSignUp() {
-    document.querySelector('#page').innerHTML = `
+  document.querySelector("#page").innerHTML = `
         <section class="sign-up">
-            <form onSubmit="signUp(event)">
-                <h2>Sign Up:</h2>
-                <fieldset>
-                  <label for="">Username: </label>
-                  <input type="text" name="name">
-                </fieldset>
-                <fieldset>
-                  <label for="">Email: </label>
-                  <input type="text" name="email">
-                </fieldset>
-                <fieldset>
-                  <label for="">Password: </label>
-                  <input type="password" name="password">
-                </fieldset>
-                <fieldset>
-                  <label htmlFor="">Avatar: </label>
-                  <input type="text" name="avatar">
-                </fieldset>
 
-                <button>Sign Up</button>
+            <form onSubmit="signUp(event)">
+
+<div class="section section-signup" style="background-image: url('https://images.unsplash.com/photo-1628968434441-d9c1c66dcde7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cG9rZW1vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60'); background-size: cover; background-position: top center; min-height: 700px;">
+  <div class="container">
+    <div class="row">
+      <div class="card card-signup" data-background-color="orange">
+        <form class="form" method="" action="">
+          <div class="card-header text-center">
+            <h3 class="card-title title-up">Sign Up</h3>
+
+          </div>
+          <div class="card-body">
+            <div class="input-group no-border">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="now-ui-icons users_circle-08"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="First Name...">
+            </div>
+            <div class="input-group no-border">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="now-ui-icons text_caps-small"></i>
+                </span>
+              </div>
+              <input type="text" placeholder="Last Name..." class="form-control">
+            </div>
+            <div class="input-group no-border">
+              <div class="input-group-prepend">
+                <span class="input-group-text">
+                  <i class="now-ui-icons ui-1_email-85"></i>
+                </span>
+              </div>
+              <input type="text" class="form-control" placeholder="Email...">
+            </div>
+          </div>
+          <div class="card-footer text-center">
+            <a href="#pablo" class="btn btn-neutral btn-round btn-lg">Get Started</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
             </form>
         </section>
-    `
+
+    `;
 }
 
-function signUp(event) {
-  event.preventDefault()
-  const form = event.target
-  const data = Object.fromEntries(new FormData(form))
+// <h2>Sign Up:</h2>
+// <fieldset>
+//   <label for="">Username: </label>
+//   <input type="text" name="name">
+// </fieldset>
+// <fieldset>
+//   <label for="">Email: </label>
+//   <input type="text" name="email">
+// </fieldset>
+// <fieldset>
+//   <label for="">Password: </label>
+//   <input type="password" name="password">
+// </fieldset>
+// <fieldset>
+//   <label htmlFor="">Avatar: </label>
+//   <input type="text" name="avatar">
+// </fieldset>
 
-  fetch('/api/users', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
+// <button>Sign Up</button>
+
+function signUp(event) {
+  event.preventDefault();
+  const form = event.target;
+  const data = Object.fromEntries(new FormData(form));
+
+  fetch("/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
   })
-    .then(res => res.json())
-    .then(userName => {
-        state.loggedInUserName = userName
-        renderPokemonList()
-        document.querySelector('#page').innerHTML = ``
-    })
-  }
+    .then((res) => res.json())
+    .then((userName) => {
+      state.loggedInUserName = userName;
+      renderPokemonList();
+      document.querySelector("#page").innerHTML = ``;
+    });
+}
